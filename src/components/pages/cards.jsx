2f217663fd3dbc8305b010a86b94e5c077bb885f
VenkatUIDev98 <venkateshmanikandan07@gmail.com>
2022-11-17 14:55:13 +0530

    improving

--- src/components/pages/cards.jsx
+++ src/components/pages/cards.jsx
@@ -3,10 +3,23 @@ import Slider from "react-slick";
 import "../assets/styles/cards.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../assets/styles/slick-slider.css"
 import axios from "axios"
 
-const Cards = () => {
+
+export default function SimpleSlider() {
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      // prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+      autoplay: true,
+      speed: 500
+    };
+// const Cards = () => {
     const [title, setTitle] = useState(null)
     const [actors, setActors] = useState(null)
     const [poster, setPoster] = useState(null)
@@ -51,10 +64,74 @@ const Cards = () => {
     }, [genre])
     
   return (
-    <Slider className='tray-area'>
+    <Slider {...settings} className='tray-area'>
         <div className='trays-container'>
             <a href='#'>Latest & Trending</a>
             <div className='trays'>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
+                <div className='tray-card'>
+                    <img src={poster} alt={title} />
+                    <div className='card-detail'>
+                        <div className='title'>{title}</div>
+                        <div className='run-time'>{runtime}</div>
+                        <div className='genre'>{genre}</div>
+                    </div>
+                </div>
                 <div className='tray-card'>
                     <img src={poster} alt={title} />
                     <div className='card-detail'>
@@ -93,5 +170,3 @@ const Cards = () => {
     </Slider>
   )
 }
-
-export default Cards
